95575f29fdd953e2e879e8c0788fa16764b37f2d
[mslm dvlpmnt] <fruit.upriver368@4wrd.cc>
2026-04-04 15:32:14 +0200

    Restyle New Post button: pill shape, filled triangle icon, lighter color-2

--- src/components/layout/MainLayout.ts
+++ src/components/layout/MainLayout.ts
@@ -1292,7 +1292,7 @@ export class MainLayout {
             <button class="btn btn--new-post">
               <svg width="24" height="24"><use href="#icon-plus"/></svg>
               New Post
-              <svg class="dropup-arrow" width="12" height="12"><use href="#icon-chevron-up"/></svg>
+              <svg class="dropup-arrow" width="14" height="14"><use href="#icon-triangle-up"/></svg>
             </button>
             <div class="new-post-dropup__menu">
               <button class="new-post-dropup__item" data-action="new-note">
