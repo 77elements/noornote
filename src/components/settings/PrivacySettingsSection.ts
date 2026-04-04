00a3722273df52620208c6b60a06526e5b9b85a0
[mslm dvlpmnt] <fruit.upriver368@4wrd.cc>
2026-04-04 21:32:59 +0200

    Add .btn-container pattern, apply btn-container--btnright to Privacy Settings

--- src/components/settings/PrivacySettingsSection.ts
+++ src/components/settings/PrivacySettingsSection.ts
@@ -106,7 +106,7 @@ export class PrivacySettingsSection extends SettingsSection {
           <p class="setting__desc">${config.description}</p>
           <p class="setting__desc ${config.id}-warning"><strong>Beta Feature:</strong> Not all Nostr clients support NIP-51 yet. If you use other clients that don't support NIP-51, you won't be able to see your private ${config.listName}.</p>
         </div>
-        <div>
+        <div class="btn-container btn-container--btnright">
           <button class="btn btn--medium" id="view-${config.id}-btn">${config.viewButtonLabel}</button>
         </div>
       </section>
